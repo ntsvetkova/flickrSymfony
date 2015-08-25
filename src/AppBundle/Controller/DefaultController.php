ba10e317221a99f0324e0e10dc90394f01406d93
Natalia Tsvetkova <nataliatss.13@gmail.com>
2015-08-25 19:31:22 +0300

    Change: working with request

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,28 +2,51 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Models\RequestParameters;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+require_once __DIR__ . '/../Models/RequestParameters.php';
+
+/**
+ * Class DefaultController
+ * @package AppBundle\Controller
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        $srclarge = '';
-        $srcthumbnail = '';
-        $title = 'Title';
-        $id = 'id';
-        $owner = 'owner';
 
-        return $this->render('flickrPhoto/photo.html.twig', array('srclarge' => $srclarge,
-            'srcthumbnail' => $srcthumbnail,
-            'title' => $title,
-            'id' => $id,
-            'owner' => $owner,
-        ));
+        $requestParameters = RequestParameters::getInstance();
+
+        $request = Request::create($requestParameters->getEndPoint() .
+            $this->get('router')->generate('homepage', $requestParameters->getRecent()));
+
+//        var_dump($request);
+
+        $test = $this->get('request_parameters');
+        var_dump($test);
+
+        return new Response($request);
+//        $srclarge = '';
+//        $srcthumbnail = '';
+//        $title = 'Title';
+//        $id = 'id';
+//        $owner = 'owner';
+//
+//        return $this->render('flickrPhoto/photo.html.twig', array('srclarge' => $srclarge,
+//            'srcthumbnail' => $srcthumbnail,
+//            'title' => $title,
+//            'id' => $id,
+//            'owner' => $owner,
+//        ));
     }
+
 }
