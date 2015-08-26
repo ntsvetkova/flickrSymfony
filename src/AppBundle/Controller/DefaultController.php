6f279204cd660d0c3ba41af245296d1039fe474b
Natalia Tsvetkova <nataliatss.13@gmail.com>
2015-08-26 17:23:25 +0300

    Change: new Action for flickr.photos.getSizes, setData function for the same code in different actions

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,17 +1,17 @@
 <?php
 
 namespace AppBundle\Controller;
-//use AppBundle\Models\FlickrPhoto;
+use AppBundle\Models\FlickrPhoto;
+use AppBundle\Models\ResponseDecode;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 
-//require_once __DIR__ . '/../Models/FlickrPhoto.php';
+require_once __DIR__ . '/../Models/FlickrPhoto.php';
+require_once __DIR__ . '/../Models/ResponseDecode.php';
 
 /**
  * Class DefaultController
@@ -19,45 +19,64 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class DefaultController extends Controller
 {
-//    private  $photo;
     /**
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @var FlickrPhoto
+     */
+    private $photo;
+
+    /**
+     * @return Response
      */
     public function indexAction()
     {
+        $responseDecode = $this->setData('getRecent', $this->photo);
+        $arrayPhotos = $responseDecode->decodeRecent();
+        foreach ($arrayPhotos as $photo) {
+            $this->forward('AppBundle:Default:final', array(
+                'photo' => $photo
+            ));
+        }
+        return $this->render('flickrPhoto/photo.html.twig', array(
+            'arrayPhotos' => $arrayPhotos
+        ));
+    }
+
+    /**
+     * @param FlickrPhoto $photo
+     * @return Response
+     */
+    public function finalAction(FlickrPhoto $photo) {
+        $responseDecode = $this->setData('getSizes', $photo);
+        $this->photo = $responseDecode->decodeSizes();
+        return new Response();
+    }
+
+    /**
+     * @param $apiMethod
+     * @param FlickrPhoto|null $photo
+     * @return mixed
+     */
+    public function setData($apiMethod, FlickrPhoto $photo = null) {
         $requestParameters = $this->get('request_parameters');
-
-        $request = Request::create($requestParameters->getEndPoint(), 'GET',
-            $requestParameters->getRecent());
-//        $requestStack = new RequestStack();
-//        $requestStack->push($request);
-        var_dump($request);
-
+        switch ($apiMethod) {
+            case 'getRecent':
+                $request = Request::create($requestParameters->getEndPoint(), 'GET',
+                    $requestParameters->getRecent());
+                break;
+            case 'getSizes':
+                $request = Request::create($requestParameters->getEndPoint(), 'GET',
+                    $requestParameters->getSizes($photo->getId()));
+                break;
+            default:
+                break;
+        }
         $sendRequest = $this->get('curl');
-        $data = $sendRequest->curlExec();
+        $data = $sendRequest->curlExec($request->getHttpHost() . $request->getRequestUri());
 
-
-
-
-
-        return new Response($request);
-        //    $this->render ('flickrPhoto/photo.html.twig', array('srclarge' => $this->photo->getSrcLarge(),
-//            'srcthumbnail' => $this->photo->getSrcThumbnail(),
-//            'title' => $this->photo->getTitle(),
-//            'id' => $this->photo->getId(),
-//            'owner' => $this->photo->getOwner(),
-//        $srclarge = '';
-//        $srcthumbnail = '';
-//        $title = 'Title';
-//        $id = 'id';
-//        $owner = 'owner';
-//
-//        return $this->render('flickrPhoto/photo.html.twig', array('srclarge' => $srclarge,
-//            'srcthumbnail' => $srcthumbnail,
-//            'title' => $title,
-//            'id' => $id,
-//            'owner' => $owner,
-//        ));
+        $responseDecode = ResponseDecode::getInstance();
+        $responseDecode->setPhoto($photo);
+        $responseDecode->setResponse($data);
+        return $responseDecode;
     }
 
 }
