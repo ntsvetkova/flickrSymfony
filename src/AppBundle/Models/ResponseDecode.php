3f65c45dd473a4d767098d84842eb1f7a9c389dd
Natalia Tsvetkova <nataliatss.13@gmail.com>
2015-08-26 17:25:59 +0300

    Change: decoding response JSON using FlickrPhoto class

--- src/AppBundle/Models/ResponseDecode.php
+++ src/AppBundle/Models/ResponseDecode.php
@@ -7,6 +7,10 @@
  */
 
 namespace AppBundle\Models;
+use AppBundle\Controller\DefaultController;
+use AppBundle\Models\FlickrPhoto;
+
+require_once __DIR__ . '/../Models/FlickrPhoto.php';
 
 /**
  * Class ResponseDecode
@@ -18,11 +22,80 @@ class ResponseDecode
      * @var
      */
     private $response;
+    /**
+     * @var FlickrPhoto
+     */
+    protected $photo;
+    /**
+     * @var array
+     */
+    protected $arrayPhotos = [];
+    /**
+     * @var
+     */
+    private static $instance;
+
+    /**
+     * Constructor
+     */
+    private function __construct() {}
+
+    /**
+     * @param FlickrPhoto $photo
+     */
+    public function setPhoto(FlickrPhoto $photo = null) {
+        $this->photo = $photo;
+    }
+
+    /**
+     * @return FlickrPhoto
+     */
+    public function getPhoto() {
+        return $this->photo;
+    }
 
     /**
      * @param $response
      */
-    public function __construct($response) {
+    public function setResponse($response) {
         $this->response = $response;
     }
+
+    /**
+     * @return mixed
+     */
+    public static function getInstance() {
+        if (empty(self::$instance)) {
+            $classname = __CLASS__;
+            self::$instance = new $classname;
+        }
+        return self::$instance;
+    }
+
+    /**
+     * @return array
+     */
+    public function decodeRecent() {
+        $obj = json_decode($this->response);
+        for ($i = 0; $i < count($obj->photos->photo); $i++) {
+            $this->photo = new FlickrPhoto($obj->photos->photo[$i]->id, $obj->photos->photo[$i]->owner, $obj->photos->photo[$i]->title);
+            array_push($this->arrayPhotos, $this->photo);
+        }
+        return $this->arrayPhotos;
+    }
+
+    /**
+     * @return FlickrPhoto
+     */
+    public function decodeSizes() {
+        $obj = json_decode($this->response);
+        $this->photo->setSrcLarge($obj->sizes->size[count($obj->sizes->size) - 1]->source);
+        for ($i = 0; $i < count($obj->sizes->size); $i++) {
+            if ($obj->sizes->size[$i]->label == "Thumbnail") {
+                $this->photo->setSrcThumbnail($obj->sizes->size[$i]->source);
+            }
+        }
+        return $this->photo;
+    }
+
 }
