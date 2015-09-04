34013df9a26755a6d57b83986d8f784163f1d9ad
Natalia Tsvetkova <nataliatss.13@gmail.com>
2015-09-04 18:47:31 +0300

    Different ways of working with elements with certain attributes

--- web/assets/js/app/index.js
+++ web/assets/js/app/index.js
@@ -1,6 +1,14 @@
 /**
  * Created by vkalachikhin on 03.09.15.
  */
-require(['app/menu', 'underscore'], function(_) {
-
+require(['app/menu', 'jquery'], function(menu, $) {
+    $( "div[class='header']" ).css('color','grey');
+    $( "div.header" )
+        .on("mouseenter", function() {
+            $( this ).css('color', 'black')
+        })
+        .on("mouseleave", function() {
+            $( this ).css('color', 'grey')
+        });
+    menu();
 });
