21242db1abbbb6940b413ee644cd30cc1d48edbd
Natalia Tsvetkova <nataliatss.13@gmail.com>
2015-09-07 15:46:03 +0300

    Installing lunetics/locale-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle()
+            new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle(),
+            new Lunetics\LocaleBundle\LuneticsLocaleBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
