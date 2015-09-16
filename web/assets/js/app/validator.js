e203d49006ee5499f975ad02b7d82176640856a9
Natalia Tsvetkova <nataliatss.13@gmail.com>
2015-09-16 10:38:39 +0300

    _.delay

--- web/assets/js/app/validator.js
+++ web/assets/js/app/validator.js
@@ -1,75 +1,88 @@
 require (["jquery", "underscore"], function ($, _) {
     $( document ).ready(function () {
-        $( "input.form-control" ).bind('keypress blur change keyup', function() {
-            var input = this;
-            $.post('/validation', {name: this.name, value: this.value})
+
+        var timer = null;
+        $("input.form-control").keydown(function() {
+            clearTimeout(timer);
+            timer = _.delay(validate, 2000, this);
+        });
+
+        function validate(input)
+        {
+            $.post('/validation', {name: input.name, value: input.value})
                 .success(function (json) {
 
-                    function removeErrorLabel() {
-                        if (!$( input ).parent().hasClass("input-group")) {
-                            $( input ).parent()
-                                .children("label[class='control-label input-sm']").remove();
-                        }
-                        else {
-                            $( input ).parentsUntil("div[class='']")
-                                .children("label[class='control-label input-sm']").remove();
-                        }
-                    }
-
                     if (json == 'Success') {
-                        $( input ).parent()
-                            .removeClass("has-error")
-                            .addClass("has-success has-feedback");
-
-                        removeErrorLabel();
 
                         var span = $('<span></span>')
                             .addClass("glyphicon glyphicon-ok form-control-feedback")
                             .attr('aria-hidden', 'true')
                             .css('top', '35px');
-                        if (!$( input ).parent().hasClass("input-group")) {
+
+                        if (!$(input).parent().hasClass("input-group")) {
+                            $(input).parent()
+                                .removeClass("has-error")
+                                .addClass("has-success has-feedback")
+                                .children("label.control-label.input-sm").remove();
+                            $(input).parent()
+                                .find("span.glyphicon").remove();
                             span.insertAfter($(input));
                         }
                         else {
-                            $( input).parent().removeClass("has-feedback");
-                            $( input ).parentsUntil("div[class='']").children("span.glyphicon").remove();
-                            $( input ).parent().parent()
-                                .addClass("has-success has-feedback");
-                            span.insertAfter($( input ).parent());
+                            $(input).parent()
+                                .removeClass("has-error")
+                                .addClass("has-success");
+                            $(input).parent().parent()
+                                .removeClass("has-error")
+                                .addClass("has-success has-feedback")
+                                .children("label.control-label.input-sm").remove();
+                            $(input).parent().parent()
+                                .find("span.glyphicon").remove();
+                            span.insertAfter($(input).parent());
+
                         }
 
                         //todo weak/strong password
 
                     }
                     else {
-                        $( input ).parent()
-                            .removeClass("has-success")
-                            .addClass("has-error has-feedback")
-                            .children("span.glyphicon").remove();
 
-                        removeErrorLabel();
-
-                        var label =  $('<label></label>')
+                        var label = $('<label></label>')
                             .addClass("control-label input-sm")
                             .text(json)
                             .css({
                                 'width': '100%',
                                 'margin-bottom': '10px'
                             });
-                        if ($( input ).parent().hasClass("input-group")) {
-                            $( input ).parentsUntil("div[class='']").children("span.glyphicon").remove();
-                            label
-                                .css('color', '#a94442')
-                                .insertAfter($( input ).parent());
+
+                        if (!$(input).parent().hasClass("input-group")) {
+                            $(input).parent()
+                                .removeClass("has-success")
+                                .addClass("has-error has-feedback")
+                                .children("label.control-label.input-sm").remove();
+                            $(input).parent()
+                                .find("span.glyphicon").remove();
+                            label.insertAfter($(input));
                         }
                         else {
-                           label.insertAfter($( input ));
+                            $(input).parent()
+                                .removeClass("has-success")
+                                .addClass("has-error");
+                            $(input).parent().parent()
+                                .removeClass("has-success")
+                                .addClass("has-error has-feedback")
+                                .children("label.control-label.input-sm").remove();
+                            $(input).parent().parent()
+                                .find("span.glyphicon").remove();
+                            label
+                                .css('color', '#a94442')
+                                .insertAfter($(input).parent());
                         }
                     }
                 })
                 .fail(function () {
                     console.log('Error: the response is not a JSON response');
                 });
-        });
+        };
     });
 });
