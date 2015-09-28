d2b99f2a52507d97d569eef550c536044676e8a0
Natalia Tsvetkova <nataliatss.13@gmail.com>
2015-09-28 15:44:07 +0300

    Built-in assets

--- web/assets/js/app/manager.js
+++ web/assets/js/app/manager.js
@@ -1,99 +1,31 @@
-define("app/manager", ["jquery", "underscore", 'app/menu', 'app/header', 'app/content'], function ($, _, Menu, Header, Content) {
+define("app/test-manager", ["jquery", "underscore", 'app/menu', 'app/header', 'app/content', 'app/publish'], function ($, _, Menu, Header, Content, Publish)  {
     var menuContainer = $("div.container-menu");
     var contentContainer = $("div.content");
     var headerContainer = $("div.header h1");
 
-    function ObserverList(){
-        this.observerList = [];
-    }
-
-    ObserverList.prototype.add = function( obj ){
-        return this.observerList.push( obj );
-    };
-
-    ObserverList.prototype.count = function(){
-        return this.observerList.length;
-    };
-
-    ObserverList.prototype.get = function( index ){
-        if( index > -1 && index < this.observerList.length ){
-            return this.observerList[ index ];
-        }
-    };
-
-    ObserverList.prototype.indexOf = function( obj, startIndex ){
-        var i = startIndex;
-
-        while( i < this.observerList.length ){
-            if( this.observerList[i] === obj ){
-                return i;
-            }
-            i++;
-        }
-
-        return -1;
-    };
-
-    ObserverList.prototype.removeAt = function( index ){
-        this.observerList.splice( index, 1 );
-    };
-
-    function Subject(){
-        this.observers = new ObserverList();
-    }
-
-    Subject.prototype.addObserver = function( observer ){
-        this.observers.add( observer );
-    };
-
-    Subject.prototype.removeObserver = function( observer ){
-        this.observers.removeAt( this.observers.indexOf( observer, 0 ) );
-    };
-
-    Subject.prototype.notify = function( context ){
-        var observerCount = this.observers.count();
-        for(var i=0; i < observerCount; i++){
-            this.observers.get(i).update( context );
-        }
-    };
-
-    function Observer(){
-        this.update = function() {};
-    }
-
     var Manager = {
-        createMenu: function() {
+        createMenu: function () {
             Menu.create();
             if (Menu.isCreated) {
-                _.extend(menuContainer, new Subject());
-                menuContainer.addObserver(contentContainer);
-                menuContainer.addObserver(headerContainer);
                 menuContainer.on('click', 'a.menu', function (e) {
                     if (this.target != 'this') {
                         e.preventDefault();
-                        menuContainer.notify(this.pathname);
+                        Publish.publish("changeContent", this.pathname);
                     }
                     else {
                         this.target = '_self';
                     }
                 });
-                this.loadContent();
-                this.changeHeader();
             }
-        },
-        loadContent: function () {
-            _.extend(contentContainer, new Observer());
-            contentContainer.update = function(link) {
-                Content.render(contentContainer, link);
-            };
-        },
-        changeHeader: function() {
-            _.extend(headerContainer, new Observer());
-            headerContainer.update = function(link) {
-                Header.render(headerContainer, link);
-            };
         }
-
     };
+
+    Publish.subscribe("changeContent", function(topic, link) {
+        Content.render(contentContainer, link);
+    });
+    Publish.subscribe("changeHeader", function (topic, header) {
+        Header.render(headerContainer, header);
+    });
+
     return Manager;
 });
